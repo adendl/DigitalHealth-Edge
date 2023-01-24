6bd88f4efc22aa1a0adb61a5577b03e01015d6ee
adel0025 <adel0025@student.monash.edu>
2023-01-25 10:29:44 +1100

    update patient-vitals-api with a try catch block

--- patient-vitals-api/index.js
+++ patient-vitals-api/index.js
@@ -2,16 +2,25 @@ const express = require('express');
 const mqtt = require('mqtt');
 const bodyParser = require('body-parser');
 
+const hostUrl = process.env.HOSTURL
+
 const app = express();
 app.use(bodyParser.json());
 
 // connect to the MQTT broker
-const client = mqtt.connect('mqtt://localhost:1883');
+const client = mqtt.connect(`${hostUrl}:1883`)
+console.log("Connected to the client");
 
 app.post('/vitals', (req, res) => {
   const vitals = req.body;
+  try {
   client.publish('vitals_topic', JSON.stringify(vitals));
-  res.status(200).send({ message: 'Vitals sent to topic' });
+  res.status(200).send({ message: vitals });
+  } catch (err)
+  {
+    console.log(err);
+    res.status(500).send({message: err});
+  }
 });
 
 app.listen(3000, () => {
