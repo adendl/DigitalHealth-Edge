22b87555ea1cd1beb6a61d02ea1b772577eaf120
adel0025 <adel0025@student.monash.edu>
2023-01-25 11:28:00 +1100

    change directory structure

---
+++ patient-vitals-kafka/index.js
@@ -0,0 +1,21 @@
+const { Kafka } = require('kafkajs');
+
+const HOSTURL = process.env.HOSTURL;
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: [HOSTURL],
+});
+const consumer = kafka.consumer({ groupId: 'test-group' });
+
+const run = async () => {
+    await consumer.connect();
+    await consumer.subscribe({ topic: 'vitals_topic'});
+  
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            const vitals = JSON.parse(message.value);
+            console.log(vitals);
+        },
+    });
+};
+run().catch(console.error);
